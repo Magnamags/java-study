32e1f55986a9365e0d30842c7a7f6297178fec54
Magnamags <151983433+Magnamags@users.noreply.github.com>
2024-04-24 00:14:15 -0400

    New Program Added

--- src/operators/Swapping.java
+++ src/operators/Swapping.java
@@ -1,4 +1,19 @@
 package operators;
 
+import java.util.Scanner;
+
 public class Swapping {
+    public static void main(String[] args) {
+        System.out.println("Enter the values of X and Y");
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println("Before swapping: "+a+" "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping: "+a+" "+b);
+
+    }
+
 }
